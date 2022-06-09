68c8e414f27e33e762781b90bd4fe0175746fb59
b-b-m <brunomotabertol@gmail.com>
2022-06-09 16:59:36 -0300

    Splash screen adicionada

--- lib/view/splash.dart
+++ lib/view/splash.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -19,26 +20,17 @@ class Splash extends State<SplashScreen> {
   Widget build(BuildContext context) {
     Timer(const Duration(seconds: 3),
         () => Navigator.popAndPushNamed(context, '/home'));
-
-    var assetsImage = const AssetImage(
-        'images/new_logo.png'); //<- Creates an object that fetches an image.
-    var image = Image(
-        image: assetsImage,
-        height: 300); //<- Creates a widget that displays an image.
+    var assetsImage = SvgPicture.asset(
+        'assets/images/splash.svg'); //<- Creates an object that fetches an image.
 
     return MaterialApp(
       home: Scaffold(
-        /* appBar: AppBar(
-          title: Text("MyApp"),
-          backgroundColor:
-              Colors.blue, //<- background color to combine with the picture :-)
-        ),*/
         body: Container(
           decoration: const BoxDecoration(color: Colors.purple),
           child: Center(
-            child: image,
+            child: assetsImage,
           ),
-        ), //<- place where the image appears
+        ),
       ),
     );
   }
